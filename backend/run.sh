0f89a1fd8f5033fd2247178c981b9b91cd4cc8b1
thegodfather005 <arkelmoghomaya@gmail.com>
2024-06-15 07:22:39 +0100

    debugging and correcting docker configurations;

--- backend/run.sh
+++ backend/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#cd /var/www
+cd /var/www
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
@@ -8,9 +8,13 @@ set -e
 # Print each command being executed (useful for debugging)
 set -x
 
-#composer install
+if [ ! -d "vendor" ]; then
+  echo "Vendor directory not found. Running composer install..."
+  composer install --prefer-dist --no-scripts --optimize-autoloader --no-interaction
+fi
+
+# Install migrations
 php artisan migrate
-#php artisan db:seed
 
 # Start PHP-FPM
 exec php-fpm
